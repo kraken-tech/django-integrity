9a3ca19e1a8c966fc15624f1000093777105bfcd
Charlie Denton <charlie@meshy.co.uk>
2024-02-09 19:43:04 +0000

    Configure running tests against the database
    This ensures we have a `DATABASES` setting, and the other requirements
    we need to talk to PostgreSQL.

--- tests/django_integrity/test_package.py
+++ tests/django_integrity/test_package.py
@@ -1,5 +1,16 @@
+import pytest
+from django import db
+
 import django_integrity as package
 
 
 def test_has_docstring() -> None:
     assert package.__doc__ is not None
+
+
+@pytest.mark.django_db
+def test_has_database() -> None:
+    """Test that the database is available."""
+    with db.connection.cursor() as cursor:
+        cursor.execute("SELECT 1")
+        assert cursor.fetchone() == (1,)
--- tests/example_app/settings.py
+++ tests/example_app/settings.py
@@ -1,3 +1,14 @@
+from environs import Env
+
+
+env = Env()
+
+
+DATABASES = {
+    "default": env.dj_db_url(
+        "DATABASE_URL", default="postgres://localhost/django_integrity"
+    ),
+}
 SECRET_KEY = "test-secret-key"
 INSTALLED_APPS = [
     "tests.example_app",
